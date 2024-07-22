19ab6a6b442ab79d25acbf7c688bb518c8a00596
STA\Sondos.Mahmoud <sondos.mahmoud@integrant.com>
2024-07-22 19:18:07 +0300

    Add video recording support to Playwright tests
    - Configured Playwright to record videos during test execution.
    - Updated After hook to attach recorded videos to Cucumber reports.

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,21 +1,32 @@
 import { BeforeAll, AfterAll, Before, After, Status } from "@cucumber/cucumber";
-import { Browser, chromium, Page } from "@playwright/test";
+import { Browser, BrowserContext, chromium, Page } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
+const fs = require("fs-extra");
 
 let browser:Browser;
+let context: BrowserContext;
 let page:Page;
 
 
 Before(async function () {
-    browser = await chromium.launch({headless:false});
-    page = await browser.newPage();
+    browser = await chromium.launch({ headless: false });
+    context = await browser.newContext({
+        recordVideo: { dir: 'test-results/videos' }, // Configure video recording
+        viewport: { width: 1920, height: 1080 }
+    });
+    page = await context.newPage();
     pageFixture.page = page;
-    await page.setViewportSize({ width: 1920, height: 1080 });
 });
 
 After(async function ({pickle}) {
+    let videoPath: string;
     const img = await pageFixture.page.screenshot({ path: `./test-results/screenshots/${pickle.name}.png`, type: "png" });
     await this.attach(img,"image/png");
+    videoPath = await pageFixture.page.video().path();
+    await this.attach(
+        fs.readFileSync(videoPath),
+        'video/webm'
+    );
     await pageFixture.page.click("//a[text()='Log out']");
     await pageFixture.page.close;
     await browser.close();
